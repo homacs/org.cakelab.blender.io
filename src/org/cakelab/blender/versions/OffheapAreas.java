5ba811dc54f46b3584b1362e3ed27338c56cfb60
homac <homac@strace.org>
2016-12-08 09:45:14 +0000

    + web: updated documentation to reflect changes due to offheap areas

--- src/org/cakelab/blender/versions/OffheapAreas.java
+++ src/org/cakelab/blender/versions/OffheapAreas.java
@@ -44,7 +44,7 @@ public class OffheapAreas {
 		// All entries have to be sorted in ascending order!
 		//
 		map.add(new Entry(  0, new String[]{"FileGlobal"}));
-		map.add(new Entry(250, new String[]{"FileGlobal", "TreeStoreElem"}));
+		map.add(new Entry(276, new String[]{"FileGlobal", "TreeStoreElem"}));
 	}
 	
 	private static final Comparator<Entry> COMPARATOR = new Comparator<Entry>() {
